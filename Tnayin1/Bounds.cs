289beaa8e57c23b9abae6e74d8e9fcbc8bb64c76
Aram Zakaryan <aram.zakaryan10@gmail.com>
2025-05-27 00:41:59 +0400

    Add files via upload

---
+++ Tnayin1/Bounds.cs
@@ -0,0 +1,59 @@
+﻿namespace ConsoleApp1;
+
+class Program
+{
+    public static int LowerBound(int[] array, int value)
+    {
+        int start = 0;
+        int end = array.Length - 1;
+        int mid;
+        int index = array.Length - 1;
+        while (start <= end)
+        {
+            mid = (start + end) / 2;
+            if (array[mid] >= value)
+            {
+                index = mid;
+                end = mid - 1;
+            }
+
+            if (array[mid] < value)
+            {
+                start = mid + 1;
+            }
+        }
+
+        return index;
+    }
+
+    public static int UpperBound(int[] array, int value)
+    {
+        int start = 0;
+        int end = array.Length - 1;
+        int mid;
+        int index = array.Length - 1;
+        while (start <= end)
+        {
+            mid = (start + end) / 2;
+            if (array[mid] > value)
+            {
+                index = mid;
+                end = mid - 1;
+            }
+
+            if (array[mid] <= value)
+            {
+                start = mid + 1;
+            }
+        }
+
+        return index;
+    }
+    
+    static void Main(string[] args)
+    {
+        int[] arr = new int[] { 1, 2, 3, 3, 3, 4, 4, 4, 5, 6, 7 };
+        Console.WriteLine(LowerBound(arr, 4));
+        Console.WriteLine(UpperBound(arr, 4));
+    }
+}
---
+++ Tnayin1/Sorting1.cs
@@ -0,0 +1,69 @@
+﻿namespace SortignAlgoritms;
+
+class Program
+{
+    public static void BubbleSort(int[] arr)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            for (int j = 0; j < arr.Length - 1 - i; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        } 
+    }
+
+    public static void SelectionSort(int[] arr)
+    {
+        int min = 0;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            min = i;
+            for (int j = i + 1; j < arr.Length; j++)
+            {
+                if (arr[j] < arr[min])
+                {
+                    min = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+    }
+
+    public static void InsertionSort(int[] arr)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            int key = i;
+            for (int j = i - 1; j >= 0; j--)
+            {
+                if (arr[j] > arr[key])
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[key];
+                    arr[key] = temp;
+                    key = j;
+                }
+            }
+        }
+    }
+
+static void Main(string[] args)
+    {
+        int[] arr = new int[] { 210, 11, 25, 14, 58, 66};
+        //BubbleSort(arr);
+        //SelectionSort(arr);
+        InsertionSort(arr);
+        foreach (var item in arr)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
