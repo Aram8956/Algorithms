665c98d1e6ffc87fb34458e133e75f78e9402ee8
Aram Zakaryan <aram.zakaryan10@gmail.com>
2025-06-09 18:51:54 +0400

    Add files via upload

---
+++ Linked List/MyList.cs
@@ -0,0 +1,115 @@
+﻿namespace Node;
+
+public class MyList<T>
+{
+    public T value;
+    public MyList<T> next;
+
+    public MyList()
+    {
+        value = default(T);
+        next = null;
+    }
+
+    private MyList(T value)
+    {
+        this.value = value;
+        next = null;
+    }
+
+    private MyList(T value, MyList<T> next)
+    {
+        this.value = value;
+        this.next = next;
+    }
+
+    public int Size()
+    {
+        int size = 0;
+        MyList<T> temp = next;
+        if (next == null)
+        {
+            return 0;
+        }
+
+        while (temp != null)
+        {
+            temp = temp.next;
+            size += 1;
+        }
+
+        return size;
+    }
+
+    public void Print()
+    {
+        MyList<T> temp = next;
+        while (temp != null)
+        {
+            Console.Write(temp.value);
+            Console.Write("->");
+            temp = temp.next;
+        }
+
+        Console.WriteLine();
+    }
+    
+    public void PushBack(T item)
+    {
+        MyList<T> newNode = new MyList<T>(item);
+        MyList<T> dummy = next;
+        if (dummy == null)
+        {
+            dummy.next = newNode;
+            return;
+        }
+        while (dummy.next != null)
+        {
+            dummy = dummy.next;
+        }
+        dummy.next = newNode;
+    }
+
+    public void PushFront(T item)
+    {
+        MyList<T> newNode = new MyList<T>(item);
+        newNode.next = next;
+        next = newNode;
+    }
+
+    public void Insert(T item, int index)
+    {
+        if (index > Size())
+        {
+            throw new IndexOutOfRangeException();
+        }
+        MyList<T> newNode = new MyList<T>(item);
+        MyList<T> dummy = this;
+        for (int i = 0; i < index; ++i)
+        {
+            dummy = dummy.next;
+        }
+        newNode.next = dummy.next;
+        dummy.next = newNode;
+    }
+
+    public void Erase(int index)
+    {
+        int length = Size();
+        if (index > length || index < 0)
+        {
+            throw new IndexOutOfRangeException();
+        }
+
+
+        MyList<T> dummy = this;
+        for (int i = 0; i < index; ++i)
+        {
+            dummy = dummy.next;
+        }
+        
+        MyList<T> newNode = dummy.next;
+        dummy.next = newNode.next;
+        newNode.next = null;
+    }
+}
---
+++ Linked List/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Node;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MyList<int> list = new MyList<int>();
+
+        Console.WriteLine("== PushFront ==");
+        list.PushFront(20);
+        list.PushFront(10);
+        list.Print();  // 10 -> 20
+
+        Console.WriteLine("== PushBack ==");
+        list.PushBack(30);
+        list.PushBack(40);
+        list.Print();  // 10 -> 20 -> 30 -> 40
+
+        Console.WriteLine("== Insert at index 2 ==");
+        list.Insert(25, 2);
+        list.Print();  // 10 -> 20 -> 25 -> 30 -> 40
+
+        Console.WriteLine("== Erase at index 3 ==");
+        list.Erase(3);
+        list.Print();  // 10 -> 20 -> 25 -> 40
+
+        Console.WriteLine("== Size ==");
+        Console.WriteLine(list.Size()); // 4
+    }
+}
